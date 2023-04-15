91a170f52621ebea5f5ec8a531f5bde2ba9ce153
Leo Junquera <leo.junquera@live.com>
2023-04-15 18:30:53 -0400

    Adding iOS app

---
+++ ios/b2c-mobile/b2c-mobile/b2c_mobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  b2c_mobileApp.swift
+//  b2c-mobile
+//
+//  Created by Leonardo Junquera on 4/15/23.
+//
+
+import SwiftUI
+
+@main
+struct b2c_mobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
